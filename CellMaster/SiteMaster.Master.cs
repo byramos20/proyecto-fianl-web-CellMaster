bb9443baf8a8679536bdac02dd539cc55c1ccfda
byramos20 <ramos102013@gmail.com>
2023-02-20 13:45:19 -0600

    Agregar archivos de proyecto.

---
+++ CellMaster/SiteMaster.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CellMaster
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void lk1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("~/WebForm1.aspx");
+
+        }
+    }
+}
---
+++ CellMaster/formulario1.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/SiteMaster.Master" AutoEventWireup="true" CodeBehind="formulario1.aspx.cs" Inherits="CellMaster.formulario1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>milagros</h1>
+</asp:Content>
